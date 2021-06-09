e74859015d3bd96a4c87009bc5051243112e662e
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-09 13:00:47 +0530

    Divided into Components

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,53 +1,52 @@
-// rcc
 import React, { Component } from "react";
+import TaskList from './TaskList';
 
 export default class Todo extends Component {
   state = {
-    taskList: ["Task-1", "Task-2", "Task-3"],
+    taskList: [],
+    currTask:""
   }
-  deleteTask(cTask) {
+
+  deleteTask= (id) => {
     let filteredTask = this.state.taskList.filter(function (task) {
-      return task !== cTask;
+      return task.id !== id;
     });
     this.setState({
-      taskList: filteredTask,
+      taskList: filteredTask
     });
   }
+
+  handleCurrTask = (e) => {
+    let currValue = e.target.value;
+    this.setState({
+      currTask: currValue
+    });
+  }
+ 
+  addTask = () => {
+    let currTask = this.state.currTask;
+    let tempArr = [...this.state.taskList, {task : currTask, id: this.state.taskList.length}]
+    this.setState({
+      taskList: tempArr,
+      currTask: "",
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="inputContainer">
-          <input type="text" />
-          <button>Submit</button>
-        </div>
-
-        <div className="taskList">
-          <ul>
-            {this.state.taskList.map((task) => {
-              return (
-                <li className="task">
-                  <p>{task}</p>
-                  <img
-                    style={styles.image}
-                    alt="delete"
-                    src="https://image.flaticon.com/icons/png/128/1632/1632602.png"
-                    onClick={() => {
-                      this.deleteTask(task);
-                    }}
-                  />
-                </li>
-              );
-            })}
-          </ul>
+          <input
+            type="text"
+            value={this.state.currTask}
+            onChange={this.handleCurrTask}
+          />
+          <button onClick={this.addTask}>Submit</button>
+          {/*Passing props from parent to children*/}
+          <TaskList taskList={this.state.taskList}
+          deleteTask={this.deleteTask}/>
         </div>
       </div>
-    );
+    )
   }
 }
-const styles = {
-  image: {
-    height: 25,
-    width: 25,
-    marginRight: 10,
-  },
-};
